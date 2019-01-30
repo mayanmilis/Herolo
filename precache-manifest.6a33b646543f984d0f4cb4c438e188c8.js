33517fdda690c9c4b5a0142e54996f840ca287a5
mayanmilis <mayanmilis16@gmail.com>
2019-01-30 18:21:10 +0200

    Updates

---
+++ precache-manifest.6a33b646543f984d0f4cb4c438e188c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd8acce52f156aa4bc99",
+    "url": "/Herolo/static/js/runtime~main.fd8acce5.js"
+  },
+  {
+    "revision": "37375cee0e3ef0c0d4ad",
+    "url": "/Herolo/static/js/main.37375cee.chunk.js"
+  },
+  {
+    "revision": "ec86d24657c9ba8a3fc2",
+    "url": "/Herolo/static/js/1.ec86d246.chunk.js"
+  },
+  {
+    "revision": "37375cee0e3ef0c0d4ad",
+    "url": "/Herolo/static/css/main.61913ba5.chunk.css"
+  },
+  {
+    "revision": "2728351d2d28417dbf3d324dffa7fb8b",
+    "url": "/Herolo/index.html"
+  }
+];
